4edafef114a9791a223b2b60ae645d6b6b2284d9
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-09-04 10:39:44 -0600

    Forms Validator

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -19,13 +19,14 @@ export class BasicPageComponent {
 
 
   public myForm: FormGroup = this.fb.group({
-    name : [''],
-    price : [0],   
-    inStorage : [0],
+    name : ['', [Validators.required, Validators.minLength(3)]],
+    price : [0,[Validators.required, Validators.min(0)]],   
+    inStorage : [0, [Validators.required, Validators.min(0)]],
   })
   constructor(private fb: FormBuilder) { }
 
   onSave(){
+    if(this.myForm.invalid) return;
     console.log(this.myForm.value);
     
   }
