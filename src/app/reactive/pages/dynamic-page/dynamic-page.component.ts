0737b5ad7d67ba7b096ea4bfe09012aa4cdac057
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-09-04 12:27:55 -0600

    Formuarios dinamicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder,FormGroup ,Validators, FormArray} from "@angular/forms";
 @Component({
   selector: 'app-dynamic-page',
   templateUrl: './dynamic-page.component.html',
@@ -8,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  // public myForm2 =new FormGroup({
+  //   favoriteGame : new FormArray([
+
+  //   ])
+  // });
+  public myForm : FormGroup = this.fb.group({
+    name: ['',[Validators.required, Validators.minLength(3)]],
+    favoritesGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ])
+  })
+
+  constructor (private fb : FormBuilder){}
+
+  get favoriteGames(){
+    return this.myForm.get('favoritesGames') as FormArray
+  }
+  onSumit(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return
+    }
+    console.log(this.myForm.valid);
+    this.myForm.reset();
+    
+  }
 }
